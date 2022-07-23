4c6860938b07f96a9729fc8e67f682d15a771041
PrajwalG7 <gaikwadaryan688@gmail.com>
2022-07-23 12:17:05 +0530

    GET request query params

--- app/src/main/java/com/example/anime/AnimeAPI.kt
+++ app/src/main/java/com/example/anime/AnimeAPI.kt
@@ -2,10 +2,14 @@ package com.example.anime
 
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface AnimeAPI {
 
     @GET("anime")
     fun getAnime(): Call<MutableList<String>>
+
+    @GET("anime")
+    fun getCharacter(@Query("title")title:String,@Query("page")pageNo:Int): Call<MutableList<AnimeCharacterList>>
 
 }
