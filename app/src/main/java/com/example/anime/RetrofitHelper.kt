bcd769dd52d56914f728aadc759de0f9fb40b9d4
PrajwalG7 <gaikwadaryan688@gmail.com>
2022-07-23 12:18:15 +0530

    added new function for pagination request

--- app/src/main/java/com/example/anime/RetrofitHelper.kt
+++ app/src/main/java/com/example/anime/RetrofitHelper.kt
@@ -7,9 +7,17 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitHelper {
 
     const val baseUrl = "https://animechan.vercel.app/api/available/"
+    const val paginationUrl="https://animechan.vercel.app/api/quotes/"
 
     fun getInstance(): Retrofit {
         return Retrofit.Builder().baseUrl(baseUrl)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    }
+
+    fun getPaginationInstance():Retrofit {
+        return Retrofit.Builder().baseUrl(paginationUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
